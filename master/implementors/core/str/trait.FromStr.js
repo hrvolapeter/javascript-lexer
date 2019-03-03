80e9184c99f9d11751776f7d74f7eea89797c01d
Travis CI User <travis@example.org>
2019-03-03 14:43:09 +0000

    Automatic Travis documentation build

--- master/implementors/core/str/trait.FromStr.js
+++ master/implementors/core/str/trait.FromStr.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["internship"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"internship/struct.IStr.html\" title=\"struct internship::IStr\">IStr</a>",synthetic:false,types:["internship::istr::IStr"]},];
+implementors["proc_macro2"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"proc_macro2/struct.TokenStream.html\" title=\"struct proc_macro2::TokenStream\">TokenStream</a>",synthetic:false,types:["proc_macro2::TokenStream"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
